100108b485d0f66ae8420bdeec742c8a9a01e8ea
etidbury <tidbury@live.com>
2018-11-09 17:10:21 +0000

    work

--- config/jest.js
+++ config/jest.js
@@ -1,5 +1,6 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
+const path = require('path')
 require('dotenv-safe').config({ path: path.join(process.cwd(),'.env'), safe: true, debug: process.env.DEBUG })
 
 module.exports = {
