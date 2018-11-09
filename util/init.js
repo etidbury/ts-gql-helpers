e319d03cd374a838d60765bd11b3f802a84571e0
etidbury <tidbury@live.com>
2018-11-09 16:19:55 +0000

    work

--- util/init.js
+++ util/init.js
@@ -21,29 +21,37 @@ const log = global['log'] || console
 
 const startServer = async ()=>{
 
-    let Prisma
+    let PrismaClient
     try {
-        Prisma = require(path.join(process.cwd(),isProd ? 'dist' : 'src', 'generated/prisma-client')).Prisma
+        PrismaClient = require(path.join(process.cwd(),isProd ? 'dist' : 'src', 'generated/prisma-client')).Prisma
     }catch(err){
         // todo: warn prisma generate etc...
         console.error(err)
         process.exit(1)
     }
+
+    const { Prisma } = require('prisma-binding')
+    const { typeDefs } = require( path.join(process.cwd(),isProd ? 'dist' : 'src','generated/prisma-client/prisma-schema') )
     
     const { resolvers } = getSchema()
 
     const { fileLoader, mergeTypes,mergeResolvers } = require('merge-graphql-schemas')
-
+    
     const server = new ApolloServer({
         typeDefs: mergeTypes([
-            require(path.join(process.cwd(),isProd ? 'dist' : 'src', 'generated/prisma-client/prisma-schema' + (isProd ? '.js' : '.ts'))).typeDefs
-            ,fs.readFileSync( path.join(process.cwd(),isProd ? 'dist' : 'src','schema.graphql') ).toString()
+            // require(path.join(process.cwd(),isProd ? 'dist' : 'src', 'generated/prisma-client/prisma-schema' + (isProd ? '.js' : '.ts'))).typeDefs
+            // fs.readFileSync( path.join(process.cwd(), 'prisma/datamodel.graphql' ) ).toString()
+            fs.readFileSync( path.join(process.cwd(),isProd ? 'dist' : 'src','schema.graphql') ).toString()
         ]),
         resolvers,
         context: req => Object.assign(
             req ,
             {
                 prisma: new Prisma({
+                    typeDefs,
+                    endpoint: PRISMA_URL
+                }),
+                db: new PrismaClient({
                     // typeDefs,
                     endpoint: PRISMA_URL
                 })
@@ -53,17 +61,23 @@ const startServer = async ()=>{
     
     const app = express()
     
-    server.applyMiddleware({ app })
+    require( path.join(process.cwd(),isProd ? 'dist' : 'src','routes') ).default(app)
     
+    server.applyMiddleware({ app })
+
     const port = PORT
     const host = HOST || '127.0.0.1'
     
     app.listen({ port: PORT },() => {
     
         const url = `http://${host}:${port}`
-    
+
+        if (!isProd){
+            require('express-routemap')(app)
+        }
+
         log.info(`🚀 Server ready at ${urljoin(url,server.graphqlPath)}`)
-    
+        
         if (USE_NOTIFIER) {
             try {
             
