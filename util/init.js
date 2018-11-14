757941b3a3c2301607889947afcbc20bfaf5cced
etidbury <tidbury@live.com>
2018-11-14 11:58:57 +0000

    work

--- util/init.js
+++ util/init.js
@@ -2,18 +2,22 @@ const path = require('path')
 require('dotenv-safe').config({ path: path.join(process.cwd(),'.env'), safe: true, debug: process.env.DEBUG })
 
 const { ApolloServer } = require('apollo-server-express')
-const { importSchema,mergeSchemas } = require('graphql-import')
-const { getSchema } = require('./graphql')
+const { importSchema } = require('graphql-import')
+// const { getSchema } = require('./graphql')
 const express = require('express')
+const { createServer } = require('http')
 const urljoin = require('url-join')
-const fs = require('fs')
+// const fs = require('fs')
+const { makeExecutableSchema } = require('graphql-tools')
 
 const {
+    DEBUG,
     NODE_ENV,
     USE_NOTIFIER,
     HOST,
     PORT,
-    PRISMA_URL
+    PRISMA_URL,
+    PRISMA_SECRET
 } = process.env
 
 const isProd = NODE_ENV === 'production'
@@ -21,54 +25,80 @@ const log = global['log'] || console
 
 const startServer = async ()=>{
 
-    let PrismaClient
+    let prismaClient
     try {
-        PrismaClient = require(path.join(process.cwd(),isProd ? 'dist' : 'src', 'generated/prisma-client')).Prisma
+        prismaClient = require(path.join(process.cwd(),isProd ? 'dist' : 'src', 'generated/prisma-client')).prisma
     }catch(err){
         // todo: warn prisma generate etc...
         console.error(err)
         process.exit(1)
     }
 
-    const { Prisma } = require('prisma-binding')
+    const { Prisma,extractFragmentReplacements } = require('prisma-binding')
     const { typeDefs } = require( path.join(process.cwd(),isProd ? 'dist' : 'src','generated/prisma-client/prisma-schema') )
     
-    const { resolvers } = getSchema()
+    // const { resolvers } = getSchema()
 
-    const { fileLoader, mergeTypes,mergeResolvers } = require('merge-graphql-schemas')
-    
-    const server = new ApolloServer({
-        typeDefs: mergeTypes([
-            // require(path.join(process.cwd(),isProd ? 'dist' : 'src', 'generated/prisma-client/prisma-schema' + (isProd ? '.js' : '.ts'))).typeDefs
-            // fs.readFileSync( path.join(process.cwd(), 'prisma/datamodel.graphql' ) ).toString()
-            fs.readFileSync( path.join(process.cwd(),isProd ? 'dist' : 'src','schema.graphql') ).toString()
-        ]),
+    const resolvers = require( path.join(process.cwd(),isProd ? 'dist' : 'src','resolvers') )
+
+    const schemaDirectives = require( path.join(process.cwd(),isProd ? 'dist' : 'src','directives') )
+
+    // const { fileLoader, mergeTypes,mergeResolvers } = require('merge-graphql-schemas')
+
+    const schema = makeExecutableSchema({
+        typeDefs: importSchema('./src/schema.graphql'),
         resolvers,
+        directiveResolvers: schemaDirectives
+    })
+
+    const server = new ApolloServer({
+        schema,
         context: req => Object.assign(
             req ,
             {
-                prisma: new Prisma({
+                db: new Prisma({
+                    fragmentReplacements: extractFragmentReplacements(resolvers),
                     typeDefs,
-                    endpoint: PRISMA_URL
+                    endpoint: PRISMA_URL,
+                    secret: PRISMA_SECRET,
+                    debug: DEBUG
                 }),
-                db: new PrismaClient({
-                    // typeDefs,
-                    endpoint: PRISMA_URL
-                })
+                prismaClient
             }
         )
     })
     
     const app = express()
     
-    require( path.join(process.cwd(),isProd ? 'dist' : 'src','routes') ).default(app)
+    const routes = require( path.join(process.cwd(),isProd ? 'dist' : 'src','routes') )
+
+    // todo: improve this logic to handle es6, commonjs and any other poss
+    if (typeof routes.default === 'function'){
+        routes.default(app)
+    }else{
+        routes(app)
+    }
     
-    server.applyMiddleware({ app })
+    server.applyMiddleware({ app, path: '/graphql' })
+
+    const httpServer = createServer(app)
+
+    // todo: make env dependant?
+    server.installSubscriptionHandlers(httpServer)
 
     const port = PORT
-    const host = HOST || '127.0.0.1'
-    
-    app.listen({ port: PORT },() => {
+    const host = HOST || '0.0.0.0'
+
+    // const { checkJwt } = require('./middleware/jwt')
+
+    // todo: update to scan all middlewares
+    app.use(require( path.join(process.cwd(),isProd ? 'dist' : 'src','middleware/auth') ))
+
+    // todo: make env dependant?
+    const { express: voyagerMiddleware } = require('graphql-voyager/middleware')
+    app.use('/voyager', voyagerMiddleware({ endpointUrl: '/graphql' }))
+
+    httpServer.listen({ port: PORT },() => {
     
         const url = `http://${host}:${port}`
 
@@ -93,6 +123,22 @@ const startServer = async ()=>{
                 log.warn('Failed to display notification. Ignoring...')
             }
         }
+
+        // todo: possibly remove?
+        // if (DEBUG){
+        //     let id = 0
+        //     const { PubSub } = require('apollo-server')
+        //     const pubsub = new PubSub()
+        //     const MESSAGE_CREATED = 'MESSAGE_CREATED'
+
+        //     setInterval(() => {
+        //         pubsub.publish(MESSAGE_CREATED, {
+        //             messageCreated: { id, content: new Date().toString() },
+        //         })
+              
+        //         id++
+        //     }, 1000)
+        // }
     
     })
 }
